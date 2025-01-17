4cf7d6487a23d9f195653ec5451cf747793151ac
Muxlisa <ymuxlisa623@gmail.come>
2025-01-17 18:10:13 +0500

    add

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,js}", // Adjust based on your HTML/JS file structure
+    "./public/**/*.html",   // Include paths where your HTML files are stored
+  ],
+  theme: {
+    extend: {}, // Customize your theme here
+  },
+  plugins: [],
+
+  content: ["./public/**/*.{html,js}"]
+};
+
+
